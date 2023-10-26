1a504c4f6057183e8a769dfca7ad8d0b9bd5f811
Leonardo-Luz <leonardoluz07@gmail.com>
2023-10-26 18:05:07 -0300

    [feat] add table props

---
+++ packages/client/src/types/employee.ts
@@ -0,0 +1,8 @@
+export type employeeProps = {
+    employeeid: number,
+    name: string,
+    cellnum: string | null,
+    email: string | null,
+    employeetypeid: number,
+    situation: boolean,
+};
---
+++ packages/client/src/types/employeetype.ts
@@ -0,0 +1,5 @@
+export type employeeTypeProps = {
+    employeetypeid: number,
+    description: string,
+    situation: boolean,
+};
