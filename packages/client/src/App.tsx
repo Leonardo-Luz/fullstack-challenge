bddfdf662a4748c58ef8c7851c57f21e70909184
Leonardo-Luz <leonardoluz07@gmail.com>
2023-10-27 19:07:58 -0300

    [feat] config routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Employeetype from './routes/EmployeeType';
+import Navbar from './components/navbar/Navbar';
+
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Employeetype/>
+      
+      <Navbar/>
+      
+      <div className='navbar-config'></div>
+      <Outlet/>
     </div>
   );
 }
