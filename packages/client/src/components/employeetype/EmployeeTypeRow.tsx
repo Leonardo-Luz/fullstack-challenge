47f8506001384607ffd910357857b75b87c1f29e
Leonardo-Luz <leonardoluz07@gmail.com>
2023-10-27 19:12:32 -0300

    [feat] add employeetype row

--- packages/client/src/components/employeetype/EmployeeTypeRow.tsx
+++ packages/client/src/components/employeetype/EmployeeTypeRow.tsx
@@ -3,18 +3,41 @@ import './table.css';
 
 type EmployeeTypeRowProps = {
     employeeTypes: employeeTypeProps[] | null;
+    filteredId?: number;
 };
 
-const EmployeeTypeRow = ( { employeeTypes }: EmployeeTypeRowProps) =>
+const EmployeeTypeRow = ( { employeeTypes , filteredId }: EmployeeTypeRowProps) =>
 {
-    const data = employeeTypes;
+    const employeeTypes_ = employeeTypes;
+
+    let notfound = false;
+
+    const FilterCheck = () =>
+    {
+        if(employeeTypes_)
+        for(let i = 0; i < employeeTypes_.length; i++)
+        {
+            if(employeeTypes_[i].employeetypeid === filteredId)            
+                return true;
+        }
+
+        return false;
+    }
 
     return(
         <table>
             <thead><tr><td className="id">id</td><td className="desc">description</td><td className="sit">situation</td></tr></thead>
             <tbody>
-            {data && data.map((data) => {
-                return <tr><td className="id">{data.employeetypeid}</td><td className="desc">{data.description}</td><td className="sit">{(data.situation && "Ativo")||(!data.situation && "Desligado")}</td></tr>
+            {employeeTypes_ && employeeTypes_.map((data) => {
+                if(filteredId === undefined)
+                    return <tr><td className="id">{data.employeetypeid}</td><td className="desc">{data.description}</td><td className="sit">{(data.situation && "Ativo")||(!data.situation && "Desligado")}</td></tr>
+                else if(!FilterCheck() && !notfound)
+                {
+                    notfound = true;
+                    return <tr><td className="id"></td><td className="desc">not found</td><td className="sit"></td></tr>
+                }
+                else if(filteredId === data.employeetypeid)
+                    return <tr><td className="id">{data.employeetypeid}</td><td className="desc">{data.description}</td><td className="sit">{(data.situation && "Ativo")||(!data.situation && "Desligado")}</td></tr>
             })}
             </tbody>
         </table>
