9145068b829400796ff4123d75ab60154b9c56c8
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-02 13:42:18 -0300

    [feat] server class

---
+++ packages/server/src/utils/server.ts
@@ -0,0 +1,53 @@
+import express, { Application } from "express";
+
+import { healthRouter , employeeRouter , employeetypeRouter } from "../routes";
+import { errorHandler, logger, rules } from "../middlewares";
+
+// import { database } from "../app";
+
+export class App {
+    public app: Application;
+
+    constructor()
+    {
+        this.app = express();
+
+        // this.app.use(cors({
+        //     credentials: true,
+        // }));
+
+        // this.app.use(compression());
+        // this.app.use(cookieParser());
+        // this.app.use(bodyParser.json());
+
+        // this.databaseSync();
+
+        this.middlewares();
+
+        this.app.use(rules);
+
+        this.routes();
+
+        this.app.use(errorHandler);
+    }
+
+    // protected databaseSync():void
+    // {
+    //     database.sequelize?.sync();
+    // }
+
+    protected middlewares(): void
+    {
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(express.json());
+        
+        this.app.use(logger);        
+    }
+
+    protected routes():void
+    {
+        this.app.use('/health', healthRouter);
+        this.app.use('/employee', employeeRouter);
+        this.app.use('/employeetype', employeetypeRouter);
+    }
+}
