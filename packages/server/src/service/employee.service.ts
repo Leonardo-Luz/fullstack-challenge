718275bfe16e5e4d2ecdf270170e9806eda7587e
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-02 13:56:44 -0300

    [wip] add crud methods

---
+++ packages/server/src/service/employee.service.ts
@@ -0,0 +1,27 @@
+import { ModelStatic } from "sequelize";
+import { employeetypeI } from "../models/employee";
+
+export const getEmployees = async ( employeeModel: ModelStatic<employeetypeI> ) =>
+{
+    return await employeeModel.findAll();
+}
+
+export const getEmployeeById  = async ( id:number , employeeModel:ModelStatic<employeetypeI> ) =>
+{
+    return await employeeModel.findByPk(id);
+}
+
+export const createEmployee  = async ( employeeModel:ModelStatic<employeetypeI> , body: any ) =>
+{
+    return employeeModel.build({ ...body }).save();
+}
+
+export const deleteEmployee  = async ( employee: any )=>
+{
+        return employee.destroy();
+}
+
+export const updateEmployee  = async ( employee: any ) =>
+{
+        return await employee.save();
+}
